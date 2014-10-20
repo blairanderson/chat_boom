535014b68ea06d41ed7d92d92efefd50cb1f8901
Blair Anderson <blair81@gmail.com>
2014-10-20 00:55:22 -0700

    event controller can capture payloads

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,21 +1,23 @@
 class EventsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
     site = Site.find_by_guid(site_id)
-    if site.events.create(payload: event_params)
+    if site && site.events.create(payload: event_payload)
       head :ok
     else
       render json: {
           message: "could not create event",
-          params: event_params
+          params: event_payload
       }, status: :fail
     end
   end
 
   def site_id
-    params.require(:event).permit(:app_id)[:app_id]
+    params.require(:site_id)
   end
 
-  def event_params
+  def event_payload
     params.require(:event).permit!
   end
 end
